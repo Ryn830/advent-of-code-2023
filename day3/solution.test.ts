6b0357293786fc2063b4f9e9241237a95905fd49
Ryan Yee <ryn830@gmail.com>
2023-12-07 20:50:56 -0800

    Update day 3 part 2 test

--- day3/solution.test.ts
+++ day3/solution.test.ts
@@ -4,16 +4,16 @@ import assert from 'node:assert/strict'
 import { parseTxtToLines } from '../prompt_helpers'
 const INPUT_PATH = './day3/input.txt'
 
-import { solution } from './solution'
+import { solution, part2 } from './solution'
 
 describe('Day 3', () => {
   it('should return the result expected from the prompt', () => {
     const mock = [
       '467..114..',
-      '..._......',
+      '...*......',
       '..35..633.',
       '......#...',
-      '617_......',
+      '617*......',
       '.....+.58.',
       '..592.....',
       '......755.',
@@ -45,5 +45,30 @@ describe('Day 3', () => {
     const actual = solution(input)
     assert.notEqual(actual, 322385) // Part numbers are not unique.
     assert.equal(actual, 538046)
+  })
+})
+
+describe.only('Day 3, Part 2', () => {
+  it('should return the correct sum of gear ratios for the mock schematic', () => {
+    const mock = [
+      '467..114..',
+      '...*......',
+      '..35..633.',
+      '......#...',
+      '617*......',
+      '.....+.58.',
+      '..592.....',
+      '......755.',
+      '...$.*....',
+      '.664.598..',
+    ]
+    const actual = part2(mock)
+    assert.equal(actual, 467835)
+  })
+
+  it('should return a solution for the provide input', () => {
+    const input = parseTxtToLines(INPUT_PATH)
+    const actual = part2(input)
+    assert.notEqual(actual, 3679697)
   })
 })
