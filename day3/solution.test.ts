091289827bb243ad260a868e7108b66302562a1a
Ryan Yee <ryn830@gmail.com>
2023-12-11 22:42:47 -0800

    Solution to day 3 part 2, finally....

--- day3/solution.test.ts
+++ day3/solution.test.ts
@@ -4,7 +4,13 @@ import assert from 'node:assert/strict'
 import { parseTxtToLines } from '../prompt_helpers'
 const INPUT_PATH = './day3/input.txt'
 
-import { solution, part2 } from './solution'
+import {
+  solution,
+  part2,
+  part2_2,
+  parseAboveOrBelowStar,
+  part2_3,
+} from './solution'
 
 describe('Day 3', () => {
   it('should return the result expected from the prompt', () => {
@@ -48,7 +54,7 @@ describe('Day 3', () => {
   })
 })
 
-describe.only('Day 3, Part 2', () => {
+describe.skip('Day 3, Part 2', () => {
   it('should return the correct sum of gear ratios for the mock schematic', () => {
     const mock = [
       '467..114..',
@@ -69,6 +75,120 @@ describe.only('Day 3, Part 2', () => {
   it('should return a solution for the provide input', () => {
     const input = parseTxtToLines(INPUT_PATH)
     const actual = part2(input)
-    assert.notEqual(actual, 3679697)
+    assert.equal(actual, 3679697) // Incorrect
+  })
+})
+
+describe.skip('Day 3, Part 2 2nd try', () => {
+  it('should return the correct sum of gear ratios for the mock schematic', () => {
+    const mock = [
+      '467..114..',
+      '...*......',
+      '..35..633.',
+      '......#...',
+      '617*......',
+      '.....+.58.',
+      '..592.....',
+      '......755.',
+      '...$.*....',
+      '.664.598..',
+    ]
+    const actual = part2_2(mock)
+    assert.equal(actual, 467835)
+  })
+
+  it('should produce the correct result for the reddit edge case test', () => {
+    const mock = [
+      '12.......*..',
+      '+.........34',
+      '.......-12..',
+      '..78........',
+      '..*....60...',
+      '78.........9',
+      '.5.....23..$',
+      '8...90*12...',
+      '............',
+      '2.2......12.',
+      '.*.........*',
+      '1.1..503+.56',
+    ]
+    // 78 * 78 + 12 * 56
+    const actual = part2_2(mock)
+    assert.equal(actual, 6756)
+  })
+
+  it('should return a solution for the provide input', () => {
+    const input = parseTxtToLines(INPUT_PATH)
+    const actual = part2_2(input)
+    assert.notEqual(actual, 53993463) // Incorrect. Missed an equal?
+    assert.notEqual(actual, 54576054) // Also incorrect.
+  })
+})
+
+describe('parseAboveOrBelowStar', () => {
+  it('should produce the correct number for each of the possible variations of positions', () => {
+    const mocks = [
+      { line: '123...', positions: [0, 1], expected: [123] },
+      { line: '123...', positions: [1, 2, 3], expected: [123] },
+      { line: '123...', positions: [2, 3, 4], expected: [123] },
+      { line: '123.456', positions: [2, 3, 4], expected: [123, 456] },
+      { line: '123.456', positions: [3, 4, 5], expected: [456] },
+      { line: '123+456', positions: [3, 4, 5], expected: [456] },
+      { line: '...456', positions: [3, 4, 5], expected: [456] },
+      { line: '...456', positions: [0, 1], expected: [] },
+    ]
+    mocks.forEach(mock => {
+      const actual = parseAboveOrBelowStar(mock.line, mock.positions)
+      assert.deepEqual(
+        actual,
+        mock.expected,
+        `${mock.line}: ${JSON.stringify(mock.positions)}. Expected: ${
+          mock.expected
+        } !== Actual: ${actual}`,
+      )
+    })
+  })
+})
+
+describe.only('Day 3 Part2 3rd type', () => {
+  it('should return the correct value for the mock and the reddit mock', () => {
+    const mock = [
+      '467..114..',
+      '...*......',
+      '..35..633.',
+      '......#...',
+      '617*......',
+      '.....+.58.',
+      '..592.....',
+      '......755.',
+      '...$.*....',
+      '.664.598..',
+    ]
+    const actual = part2_3(mock)
+    assert.equal(actual, 467835)
+
+    const mock1 = [
+      '12.......*..',
+      '+.........34',
+      '.......-12..',
+      '..78........',
+      '..*....60...',
+      '78.........9',
+      '.5.....23..$',
+      '8...90*12...',
+      '............',
+      '2.2......12.',
+      '.*.........*',
+      '1.1..503+.56',
+    ]
+    // 78 * 78 + 12 * 56 = 6756
+    const actual1 = part2_3(mock1)
+    assert.equal(actual1, 6756)
+  })
+
+  it('should return the correct answer for the input', () => {
+    const input = parseTxtToLines(INPUT_PATH)
+    const actual = part2_3(input)
+    assert.equal(actual, 81709807)
   })
 })
--- day3/solution.ts
+++ day3/solution.ts
@@ -1,3 +1,8 @@
+import * as fs from 'fs'
+const INPUT_PATH = './day3/input.txt'
+
+export const parseTxtToLines = (path: string): string[] =>
+  fs.readFileSync(path, { encoding: 'utf-8' }).split('\n')
 /**
   Example grid:
     467..114..
@@ -171,4 +176,203 @@ function parseLineForStarPositions(row, line) {
     }
     return stars
   }, [])
+}
+
+export const part2_2 = (input: string[]): number => {
+  const stars: Record<string, string[]> = input
+    .map((line, row) => parseLineForStarPositions(row, line))
+    .flat()
+    .reduce((acc, position) => {
+      let [r, c] = position
+      r = r < 10 ? `0${r}` : r
+      c = c < 10 ? `0${c}` : c
+      return Object.assign({}, acc, {
+        [`${r},${c}`]: getParimeterIndicies(r, c)
+          .filter(([row, col]) => isValidPosition(input, row, col))
+          .map(p => p.join()),
+      })
+    }, {})
+  const numberMap = createNumberMap(input)
+
+  return Object.values(stars)
+    .map(perimeter =>
+      perimeter
+        .map(position =>
+          Object.keys(numberMap).filter(key => key.includes(position)),
+        )
+        .flat()
+        .reduce((set, numKey) => set.add(numKey), new Set<string>()),
+    )
+    .filter(set => set.size === 2)
+    .map(set => {
+      return [...set.values()].reduce(
+        (acc, numKey) => acc * numberMap[numKey],
+        1,
+      )
+    })
+    .reduce((sum, value) => sum + value, 0)
+}
+
+// Returns true if a position is not a number or a '.'
+function positionContainsStar(grid: string[], position: number[]) {
+  const [r, c] = position
+  return grid[r].charAt(c) === '*'
+}
+
+function createNumberMap(grid: string[]): Record<string, number> {
+  const numberMap = grid.reduce((acc, line, rowIndex) => {
+    const numbers = parseLineForNumberPositions(rowIndex, line)
+    return { ...acc, ...numbers }
+  }, {})
+  return numberMap
+}
+
+function parseLineForNumberPositions(
+  row: number,
+  line: string,
+): Record<string, number> {
+  let i = 0
+  let current: [string, string] = ['', '']
+  let result: Record<string, number> = {}
+  let rowString = row < 10 ? `0${row}` : `${row}`
+  while (i <= line.length) {
+    if (/\d/.test(line[i])) {
+      const colString = i < 10 ? `0${i}` : `${i}`
+      current[0] = current[0].length ? current[0] + line[i] : line[i]
+      current[1] = current[1].length
+        ? `${current[1]};${rowString},${colString}`
+        : `${rowString},${colString}`
+    } else {
+      if (current[0].length) {
+        result = Object.assign({}, result, {
+          [current[1]]: parseInt(current[0]),
+        })
+        current = ['', '']
+      }
+    }
+    i++
+  }
+  return result
+}
+
+export const part2_3 = (input: string[]): number => {
+  const result: number[] = []
+  input.forEach((line, row) => {
+    line.split('').forEach((char, col) => {
+      if (/\*/.test(char)) {
+        const { up, down, left, right } = getValidPositions(input, [row, col])
+        const gears = [
+          ...parseAboveOrBelowStar(input[row - 1], up),
+          ...parseAboveOrBelowStar(input[row + 1], down),
+          ...[
+            left && /\d/.test(input[row].charAt(left))
+              ? findNumber(input[row], left, -1)
+              : [],
+            right && /\d/.test(input[row].charAt(right))
+              ? findNumber(input[row], right, 1)
+              : [],
+          ].flat(),
+        ]
+        if (gears.length === 2) {
+          result.push(gears[0] * gears[1])
+        }
+      }
+    })
+  })
+  return result.reduce((sum, ratio) => sum + ratio, 0)
+}
+
+// 0  1  2  3  4  5  6  7  8  9 10 11
+// 8  .  .  .  9  0  *  1  2  .  .  .
+
+// ..78........
+// ..*....60...
+// 78.........9
+export function parseAboveOrBelowStar(
+  line: string,
+  positions: number[],
+): number[] {
+  const hasDigits = positions.map(p => /\d/.test(line.charAt(p)))
+  if (hasDigits.every(v => v === false)) {
+    return []
+  }
+  if (hasDigits.every(v => v === true)) {
+    return [findNumber(line, positions[0], 1)]
+  }
+  if (hasDigits.length === 2) {
+    const numbers = positions.reduce<number[]>((acc, position) => {
+      const left = findNumber(line, position, -1)
+      const right = findNumber(line, position, 1)
+      return acc.concat(isNaN(left) ? 0 : left, isNaN(right) ? 0 : right)
+    }, [])
+    return [Math.max(...numbers)]
+  }
+  if (!hasDigits[0] && hasDigits[1] && !hasDigits[2]) {
+    return [findNumber(line, positions[1], -1)]
+  }
+  if (hasDigits[0] && !hasDigits[1] && !hasDigits[2]) {
+    return [findNumber(line, positions[0], -1)]
+  }
+  if (hasDigits[0] && hasDigits[1] && !hasDigits[2]) {
+    return [findNumber(line, positions[1], -1)]
+  }
+  if (!hasDigits[0] && !hasDigits[1] && hasDigits[2]) {
+    return [findNumber(line, positions[positions.length - 1], 1)]
+  }
+  if (!hasDigits[0] && hasDigits[1] && hasDigits[2]) {
+    return [findNumber(line, positions[1], 1)]
+  }
+  if (hasDigits[0] && !hasDigits[1] && hasDigits[2]) {
+    const left = findNumber(line, positions[0], -1)
+    const right = findNumber(line, positions[positions.length - 1], 1)
+    return [left, right]
+  }
+  return []
+}
+
+function findNumber(line: string, position: number, direction: number): number {
+  let result = ''
+  if (Math.sign(direction) === -1) {
+    for (let i = position; /[\d]/.test(line[i]) && i >= 0; i--) {
+      result = `${line[i]}${result}`
+    }
+  } else if (Math.sign(direction) === 1) {
+    for (let i = position; /[\d]/.test(line[i]) && i <= line.length; i++) {
+      result = `${result}${line[i]}`
+    }
+  }
+  return parseInt(result)
+}
+
+type Perimeter = {
+  up: number[]
+  down: number[]
+  left: number | null
+  right: number | null
+}
+
+function getValidPositions(
+  grid: string[],
+  position: [number, number],
+): Perimeter {
+  const [row, col] = position
+  let perimeter: Perimeter = { up: [], down: [], left: null, right: null }
+  perimeter.up = [
+    [row - 1, col - 1],
+    [row - 1, col],
+    [row - 1, col + 1],
+  ]
+    .filter(([r, c]) => isValidPosition(grid, r, c))
+    .map(([_, c]) => c)
+  perimeter.down = [
+    [row + 1, col - 1],
+    [row + 1, col],
+    [row + 1, col + 1],
+  ]
+    .filter(([r, c]) => isValidPosition(grid, r, c))
+    .map(([_, c]) => c)
+  perimeter.left = isValidPosition(grid, row, col - 1) ? col - 1 : null
+  perimeter.right = isValidPosition(grid, row, col + 1) ? col + 1 : null
+
+  return perimeter
 }
