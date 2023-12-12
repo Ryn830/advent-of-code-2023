26e438d1d81a8bd24923637df5f8eda4efbca08c
Ryan Yee <ryn830@gmail.com>
2023-12-12 15:47:37 -0800

    Add solution to day 4 part 2

--- day4/solution.test.ts
+++ day4/solution.test.ts
@@ -4,7 +4,7 @@ import assert from 'node:assert/strict'
 import { parseTxtToLines } from '../prompt_helpers'
 const INPUT_PATH = './day4/input.txt'
 
-import { solution } from './solution'
+import { solution, solutionPart2 } from './solution'
 
 describe('Day 4', () => {
   it('should return the result expected from the prompt', () => {
@@ -24,5 +24,26 @@ describe('Day 4', () => {
     const input = parseTxtToLines(INPUT_PATH)
     const actual = solution(input)
     assert.equal(actual, 25231)
+  })
+})
+
+describe('Day 4, Part 2', () => {
+  it('should replicate subsequent cards when there is a match', () => {
+    const mock = [
+      'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53', // 4
+      'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19', // 2
+      'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1', // 2
+      'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83', // 1
+      'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36', // 0
+      'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11', // 0
+    ]
+    const expected = 30
+    assert.equal(solutionPart2(mock), expected)
+  })
+
+  it('should return a solution for the provide input', () => {
+    const input = parseTxtToLines(INPUT_PATH)
+    const actual = solutionPart2(input)
+    assert.equal(actual, 9721255)
   })
 })
--- day4/solution.ts
+++ day4/solution.ts
@@ -1,10 +1,10 @@
 /**
-  Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-  Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-  Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-  Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-  Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-  Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+  Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53 (4)
+  Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19 (2)
+  Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1 (2)
+  Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83 (1)
+  Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36 (0)
+  Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11 (0)
 
   - Column 1: Winning numbers
   - Column 2: Numbers you have
@@ -14,25 +14,41 @@
     (0W = 0, 1W = 1, 2W = 2, 3W = 4, 4W = 8, 5W = 16)
   - Sum all the points together to get your result
  */
+const countMatches = (line: string): number => {
+  const [winningNumbers, choosenNumbers] = line
+    .slice(line.indexOf(':') + 1)
+    .split('|')
+    .map(numString =>
+      numString
+        .trim()
+        .split(/\s+/)
+        .map(num => parseInt(num)),
+    )
+  const matches = choosenNumbers.reduce(
+    (matches, num) => matches + (winningNumbers.includes(num) ? 1 : 0),
+    0,
+  )
+  return matches
+}
 
 export const solution = (input: string[]): number => {
   return input
     .map(line => {
-      const [winningNumbers, choosenNumbers] = line
-        .slice(input[0].indexOf(':') + 1)
-        .split('|')
-        .map(numString =>
-          numString
-            .trim()
-            .split(/\s+/)
-            .map(num => parseInt(num)),
-        )
-      const matches = choosenNumbers.reduce(
-        (matches, num) => matches + (winningNumbers.includes(num) ? 1 : 0),
-        0,
-      )
-
+      const matches = countMatches(line)
       return matches === 0 ? 0 : matches === 1 ? 1 : Math.pow(2, matches - 1)
     })
     .reduce((sum, points) => sum + points, 0)
+}
+
+export const solutionPart2 = (input: string[]): number => {
+  const copies: number[] = Array.from<number>({ length: input.length }).fill(1)
+
+  input.forEach((card, cardIndex) => {
+    const matches = countMatches(card)
+    for (let i = 0; i < matches; i++) {
+      copies[i + 1 + cardIndex] += copies[cardIndex]
+    }
+  })
+
+  return copies.reduce((sum, val) => sum + val, 0)
 }
